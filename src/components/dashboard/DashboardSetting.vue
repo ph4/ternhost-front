95b899582f4e7c9e0600d026e6b976fbfc8622b8
0xGod <ddak1914@gmail.com>
2024-03-15 02:17:20 +0200

    Bacon Seed

--- src/components/dashboard/DashboardSetting.vue
+++ src/components/dashboard/DashboardSetting.vue
@@ -9,73 +9,115 @@
         <div class="setting__body-security">
           <ul class="security">
             <li class="security__item">
+              <input type="file" class="hidden" name="avatar" ref="avatar" @change="this.upload" />
+
               <h1>Avatar</h1>
-              <base-button class="btn-secondary">Add</base-button>
+              <base-button class="btn-secondary" @click="this.$refs.avatar.click()">Add</base-button>
             </li>
             <li class="security__item">
               <h1>Confirmation By Email</h1>
-              <base-button class="btn-green">Enabled</base-button>
+              <base-button :class="{ 'btn-green': this.isConfirmEmail, 'btn-red': !this.isConfirmEmail }">{{
+                this.isConfirmEmail ? 'Enabled' : 'Disabled'
+              }}</base-button>
             </li>
             <li class="security__item">
               <h1>Confirmation By Phone</h1>
-              <base-button class="btn-green">Enabled</base-button>
+              <base-button :class="{ 'btn-green': this.isConfirmPhone, 'btn-red': !this.isConfirmPhone }">{{
+                this.isConfirmPhone ? 'Enabled' : 'Disabled'
+              }}</base-button>
             </li>
             <li class="security__item">
               <h1>Two-Factor Authentication</h1>
-              <base-button class="btn-red">Disabled</base-button>
+              <base-button :class="{ 'btn-green': this.isTwoAuth, 'btn-red': !this.isTwoAuth }">{{
+                this.isTwoAuth ? 'Enabled' : 'Disabled'
+              }}</base-button>
             </li>
           </ul>
         </div>
-        <form class="setting__body-form">
+        <Form
+          class="setting__body-form"
+          @submit="this.onSubmit"
+          @invalid-submit="this.onInvalidSubmit"
+          :validation-schema="schema"
+        >
           <div class="box grid-2">
             <div class="form-part">
               <label>First Name</label>
-              <input type="text" placeholder="Enter your First Name" />
+
+              <Field name="firstName" v-model="this.firstName">
+                <input type="text" placeholder="Enter your First Name" v-model="this.firstName" />
+              </Field>
             </div>
             <div class="form-part">
               <label>Last Name</label>
-              <input type="text" placeholder="Enter your Last Name" />
+
+              <Field name="lastName" v-model="this.lastName">
+                <input type="text" placeholder="Enter your Last Name" v-model="this.lastName" />
+              </Field>
             </div>
           </div>
           <div class="box grid-2">
             <div class="form-part">
               <label>Email</label>
-              <input type="email" placeholder="Enter your Email" />
+
+              <Field name="email" v-model="this.email">
+                <input type="text" placeholder="Enter your Email" v-model="this.email" />
+              </Field>
             </div>
             <div class="form-part">
               <label>Phone</label>
-              <input type="text" placeholder="Enter your Phone" />
+
+              <Field name="phone" v-model="this.phone">
+                <input type="text" placeholder="Enter your Phone" v-model="this.phone" />
+              </Field>
             </div>
           </div>
           <div class="box grid-2">
             <div class="form-part">
-              <label>Address</label>
-              <input type="text" placeholder="Enter your Adress" />
+              <label>Street</label>
+
+              <Field name="street" v-model="this.street">
+                <input type="text" placeholder="Enter your street" v-model="this.street" />
+              </Field>
             </div>
             <div class="form-part">
               <label>Country</label>
-              <input type="text" placeholder="Enter your Country" />
+
+              <Field name="country" v-model="this.country">
+                <input type="text" placeholder="Enter your Country" v-model="this.country" />
+              </Field>
             </div>
           </div>
           <div class="box grid-3">
             <div class="form-part">
               <label>City</label>
-              <input type="text" placeholder="Enter your City" />
+
+              <Field name="city" v-model="this.city">
+                <input type="text" placeholder="Enter your City" v-model="this.city" />
+              </Field>
             </div>
             <div class="form-part">
               <label>State</label>
-              <input type="text" placeholder="Enter your State or Province" />
+
+              <Field name="state" v-model="this.state">
+                <input type="text" placeholder="Enter your State or Province" v-model="this.state" />
+              </Field>
             </div>
             <div class="form-part">
               <label>Zip Code</label>
-              <input type="text" placeholder="Enter your Zip Code" />
+
+              <Field name="zipCode" v-model="this.zipCode">
+                <input type="number" placeholder="Enter your zip code" v-model="this.zipCode" />
+              </Field>
             </div>
+
+            <button type="submit" ref="enterForm" class="hidden"></button>
           </div>
-        </form>
+        </Form>
       </div>
       <footer class="setting__footer">
         <base-button class="btn-white" @click="this.toggle">Cancel</base-button>
-        <base-button class="btn-white">Save</base-button>
+        <base-button class="btn-white" @click="this.$refs.enterForm.click()">Save</base-button>
       </footer>
     </div>
   </section>
@@ -84,22 +126,63 @@
 <script>
 import BaseButton from '@/components/UI/BaseButton.vue';
 
+import { Form, Field } from 'vee-validate';
 import { gsap } from 'gsap';
+
+import * as yup from 'yup';
 
 export default {
   name: 'DashboardSetting',
   components: {
     BaseButton,
+    Form,
+    Field,
   },
   data() {
     return {
       isShow: false,
+      isConfirmEmail: false,
+      isConfirmPhone: false,
+      isTwoAuth: false,
+
+      avatar: undefined,
+      firstName: 'John',
+      lastName: 'Snow',
+      email: 'mr.tretyak15@gmail.com',
+      phone: '+1 (828) 450 15 87',
+      street: '10 Ascot Point Cir 205',
+      country: 'United State',
+      city: 'Asheville NC',
+      state: 'New York',
+      zipCode: 10001,
     };
+  },
+  computed: {
+    schema() {
+      return yup.object({
+        avatar: yup.string(),
+        firstName: yup.string(),
+        lastName: yup.string(),
+        email: yup.string(),
+        phone: yup.string(),
+        street: yup.string(),
+        country: yup.string(),
+        city: yup.string(),
+        state: yup.string(),
+        zipCode: yup.number(),
+      });
+    },
   },
   mounted() {
     this.$emitter.on('_dashboard_-show-modal', this.toggle);
   },
   methods: {
+    onSubmit(values) {
+      console.log('[onSubmit]: ', values);
+    },
+    onInvalidSubmit({ errors }) {
+      console.log('[onInvalidSubmit]: ', errors);
+    },
     toggle() {
       const tl = gsap.timeline();
 
@@ -141,8 +224,37 @@ export default {
 
       this.isShow = !this.isShow;
     },
-    cancel() {},
-    save() {},
+    upload() {
+      console.log('[upload]: ');
+
+      const files = this.$refs.avatar.files;
+
+      if (files) {
+        const formData = new FormData(); // avatar
+        formData.append('avatar', files[0]);
+      }
+    },
+    confirmEmail() {
+      console.log('[confirmEmail]: ');
+
+      if (!this.isConfirmEmail) {
+        // Confirm email here...
+      }
+    },
+    confirmPhone() {
+      console.log('[confirmPhone]: ');
+
+      if (!this.isConfirmPhone) {
+        // Confirm phone here...
+      }
+    },
+    twoAuth() {
+      console.log('[twoAuth]: ');
+
+      if (!this.isTwoAuth) {
+        // Two auth here...
+      }
+    },
   },
 };
 </script>
